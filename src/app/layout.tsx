2f4840b28288dbd92115546c5c6fb64d2596850d
Felix Arjuna <felixarjuna.de@yahoo.com>
2025-03-10 09:51:51 +0100

    feature: add homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,12 +10,20 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const departureMono = localFont({
+  variable: "--font-departure-mono",
+  src: [{ path: "./../../public/fonts/departuremono.otf", style: "normal" }],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={`${GeistSans.variable} ${departureMono.variable}`}
+    >
+      <body className="font-mono">{children}</body>
     </html>
   );
 }
---
+++ src/components/glitch-title.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Random, Range } from "kdim";
+import { version } from "package.json";
+import React from "react";
+
+const BATTERY = "BATTERY";
+const PASS = "PASS";
+const COGNATES: Record<string, string> = {
+  E: "3ΣΞ€Ǝ",
+  A: "Λ",
+  R: "2₹",
+  T: "7",
+  U: "Ʉ",
+  " ": "_",
+  O: "0",
+  N: "Ɲ",
+} as const;
+
+export default function GlitchTitle() {
+  const MIN_DELAY = 400;
+  const MAX_DELAY = 2000;
+  const GLITCH_CHANCE = 0.1;
+  const GLITCH_DELAY = 30;
+
+  const [battery, setBattery] = React.useState<string>(BATTERY);
+  const [pass, setPass] = React.useState<string>(PASS);
+
+  function glitchWord(original: string, odds: number[]): string {
+    const swaps = Random.sample(odds)!;
+    if (swaps === 0) return original;
+
+    const glitched = [...original];
+    const opts =
+      original.length === 1
+        ? [0]
+        : Random.permutation(Range.of(original.length));
+    for (let i = 0; i < swaps; i++) {
+      const random = opts[i]!;
+      glitched[random] = Random.sample<string>([
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        ...(COGNATES[original[random]!] ?? original[random])!,
+      ])!;
+    }
+
+    return glitched.join("");
+  }
+
+  React.useEffect(() => {
+    (function d() {
+      setBattery(glitchWord(BATTERY, [0, 0, 0, 1, 1, 2, 2, 2, 3]));
+      if (Math.random() < GLITCH_CHANCE) {
+        const delay = Random.natural(MAX_DELAY - MIN_DELAY) + MIN_DELAY;
+        const glitched = glitchWord(BATTERY, [2, 3, 4, 4, 5, 5, 5, 6, 6]);
+        setTimeout(() => setBattery(BATTERY), delay);
+        setTimeout(() => setBattery(glitched), delay + GLITCH_DELAY);
+        setTimeout(() => setBattery(BATTERY), delay + GLITCH_DELAY * 2);
+        setTimeout(() => setBattery(glitched), delay + GLITCH_DELAY * 3);
+        setTimeout(d, delay + GLITCH_DELAY * 4);
+      } else {
+        setTimeout(d, Random.natural(MAX_DELAY - MIN_DELAY) + MIN_DELAY);
+      }
+    })();
+    (function s() {
+      setTimeout(s, Random.natural(MAX_DELAY - MIN_DELAY) + MIN_DELAY);
+    })();
+    (function m() {
+      setPass(glitchWord(PASS, [0, 0, 0, 1, 1, 2, 2]));
+      if (Math.random() < GLITCH_CHANCE) {
+        const delay = Random.natural(MAX_DELAY - MIN_DELAY) + MIN_DELAY;
+        const glitched = glitchWord(BATTERY, [1, 1, 2, 2, 3, 3]);
+        setTimeout(() => setBattery(BATTERY), delay);
+        setTimeout(() => setBattery(glitched), delay + GLITCH_DELAY);
+        setTimeout(() => setBattery(BATTERY), delay + GLITCH_DELAY * 2);
+        setTimeout(() => setBattery(glitched), delay + GLITCH_DELAY * 3);
+        setTimeout(m, delay + GLITCH_DELAY * 4);
+      } else {
+        setTimeout(m, Random.natural(MAX_DELAY - MIN_DELAY) + MIN_DELAY);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className="gri">
+      <h1 className="flex items-center text-8xl transition-all">
+        <span>{battery}</span>
+        <span>_</span>
+        <span>{pass}</span>
+      </h1>
+      <p className="ml-2 inline-flex bg-green-400 p-1 text-sm text-black">
+        v{version}
+      </p>
+    </div>
+  );
+}
